635dcb7969aedb1c9276784b2be2a6b6c9d33a3b
MikhailIgnatovich <bulich198922@gmail.com>
2021-07-13 22:50:21 +0300

    В panel.xml добавил разметку

---
+++ app/src/main/java/com/bulich/misha/workwithroom/tabs/Panel.kt
@@ -0,0 +1,20 @@
+package com.bulich.misha.workwithroom.tabs
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bulich.misha.workwithroom.R
+
+
+class Panel : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.panel, container, false)
+    }
+}
